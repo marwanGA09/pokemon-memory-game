80b4d471c9ab9988dd49c5e6261974cd2ca8b00c
Adem Kedir <ademkedir724@gmail.com>
2024-06-03 01:57:28 +0300

    style coloring, score

--- src/App.jsx
+++ src/App.jsx
@@ -135,8 +135,12 @@ function ScoreBoard({ score, highScore }) {
   return (
     <div className="score-board">
       <h3>Memory Game</h3>
-      <p>Score: {score}</p>
-      <p>High Score: {highScore}</p>
+      <p>
+        Score: <span>{score}</span>
+      </p>
+      <p>
+        High Score: <span className="high-score">{highScore}</span>
+      </p>
     </div>
   );
 }
