0fd237b24fdac952b43971e696f781c56029787b
Adem Kedir <ademkedir724@gmail.com>
2024-06-03 10:56:07 +0300

    add awesomeFont icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,8 @@
 import { useEffect, useRef, useState } from 'react';
 import shuffle from '../randomize';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faXTwitter } from '@fortawesome/free-brands-svg-icons';
+import { faMailBulk } from '@fortawesome/free-solid-svg-icons';
 // MAX ID 1020
 
 function ResultDisplay({
@@ -339,9 +342,15 @@ function Footer() {
     <div className="footer">
       <p>By Adem KG</p>
       <div className="links">
-        <a href="#">G</a>
-        <a href="#">L</a>
-        <a href="#">T</a>
+        <a href="mailto:ademkedir724@gmail.com">
+          <FontAwesomeIcon icon={faMailBulk} />
+        </a>
+        <a href="#">
+          <FontAwesomeIcon icon={faLinkedin} />
+        </a>
+        <a href="#">
+          <FontAwesomeIcon icon={faXTwitter} />
+        </a>
       </div>
     </div>
   );
