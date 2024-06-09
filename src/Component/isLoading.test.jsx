46a1c550a019fcd71884f43562b814313bc5d232
Adem Kedir <ademkedir724@gmail.com>
2024-06-09 10:09:10 +0300

    test is loading Component

---
+++ src/Component/isLoading.test.jsx
@@ -0,0 +1,17 @@
+import { render, screen } from '@testing-library/react';
+import { describe, expect, test } from 'vitest';
+import { IsLoading } from './IsLoading';
+
+describe('isLoading test', () => {
+  test('found in the document', () => {
+    render(<IsLoading />);
+    const imageComponent = screen.getByRole('img');
+    screen.debug(imageComponent);
+    expect(imageComponent).toBeInTheDocument();
+  });
+
+  test('Take snap shot', () => {
+    render(<IsLoading />);
+    expect(screen.getByRole('img')).toMatchSnapshot();
+  });
+});
